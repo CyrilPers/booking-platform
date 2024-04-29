eda2b384dc2b1c7ba518d6371d59e58a8ad18e23
Cyril <cyril.personne@outlook.fr>
2024-04-29 14:06:59 +0200

    company page

--- front/src/app/company/company.module.ts
+++ front/src/app/company/company.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from "@angular/common";
 import { RouterModule, Routes } from "@angular/router";
 import { FormsModule } from "@angular/forms";
 import { CompanyComponent } from "./company.component";
+import { CompanyNavBarComponent } from "./company-nav-bar/company-nav-bar.component";
 
 const companyRoutes: Routes = [
   { path: "company", component: CompanyComponent },
@@ -10,7 +11,8 @@ const companyRoutes: Routes = [
 
 @NgModule({
   declarations: [
-    CompanyComponent
+    CompanyNavBarComponent,
+    CompanyComponent,
   ],
   imports: [CommonModule, FormsModule, RouterModule.forChild(companyRoutes)],
   providers: [],
