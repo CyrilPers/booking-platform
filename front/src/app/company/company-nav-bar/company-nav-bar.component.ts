f1d7b96e853286cbbcb050846397437c25772364
Cyril <cyril.personne@outlook.fr>
2024-04-30 08:02:44 +0200

    company services

---
+++ front/src/app/company/company-nav-bar/company-nav-bar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-company-nav-bar',
+  templateUrl: './company-nav-bar.component.html',
+  styleUrl: './company-nav-bar.component.css'
+})
+export class CompanyNavBarComponent {}
